4e38e7816f5afafcbe92afa46d6c50ad3cf7f34c
ManirathinamSoundirarajan <manirathinam.soundirarajan@sgkinc.com>
2022-08-27 16:02:40 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='its a wafer prediction projects using mlops',
+    author='ManirathinamSoundirarajan',
+    license='MIT',
+)
